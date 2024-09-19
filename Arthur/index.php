0fb4d7507cba74ba93f0eb972eb60976a8486460
Iago Fragnan <n3rdydzn@hotmail.com>
2024-09-19 08:17:46 -0300

    Update index.php

--- Arthur/index.php
+++ Arthur/index.php
@@ -1,11 +1,5 @@
 <?php
 
-
-$db_name = "arthur_miaudote";
-
-$con = new PDO("mysql:host=localhost;dbname=" . $db_name, 'root', '');
-$con->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
-
 if(!isset($_FILES['upload'])){
     echo(json_encode(['status' => 'erro', 'mensagem' => 'Imagem não enviada na requisição.']));
     die();
@@ -23,14 +17,11 @@ if(!is_dir($directory)){
 
 $ext_file = pathinfo($received_file['name'], PATHINFO_EXTENSION);
 
-
 //  FILE_8yasd89fyas879f.png
 $uniq_name = uniqid('file_') . '.' . $ext_file;
 $final_path = $directory . $uniq_name;
 
 if(move_uploaded_file($received_file['tmp_name'], $final_path)){
-    $smt = $con->prepare("INSERT INTO imagens VALUES (default, '". $uniq_name ."')");
-    $smt->execute();
     echo(json_encode(['status'=>'sucesso', 'arquivo' => 'https://marciossupiais.shop/arthur_miaudote/uploads/' . $uniq_name]));
 }
 else{
